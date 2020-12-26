1a68ded343443532fbc4318ba07197d5e2a13a1c
welington-pre <g.modules28@gmail.com>
2020-12-26 12:22:41 -0300

    Melhorias no layout.

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-    // baseURL: 'http://localhost:8081/loja/'
-    baseURL: 'https://produtos-apirest-backend.herokuapp.com/loja/'
+    baseURL: 'http://localhost:8081/loja/'
+    // baseURL: 'https://produtos-apirest-backend.herokuapp.com/loja/'
 })
